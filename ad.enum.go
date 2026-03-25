c10cff744a5eda0434fa0536a8ba802c2bd25d70
Romain <romain@orixamedia.com>
2026-03-25 08:46:51 +0100

    doc: add tiktok doc for ad cta enums

--- ad.enum.go
+++ ad.enum.go
@@ -7,6 +7,7 @@ const (
 	AdDynamicFormat_UNSET            AdDynamicFormat = "UNSET"            // Catalog-based videos
 )
 
+// AdCta https://business-api.tiktok.com/gateway/docs/index?identify_key=c0138ffadd90a955c1f0670a56fe348d1d40680b3c89461e09f78ed26785164b&language=ENGLISH&doc_id=1737174886619138#item-link-Call-to-action%20(call_to_action)
 type AdCta string
 
 const (
